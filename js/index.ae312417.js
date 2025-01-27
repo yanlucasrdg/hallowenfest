4867cea93c0b65b84227f1e25e1458e90a85321a
Yan Lucas Ribeiro <99301609+yanlucasrdg@users.noreply.github.com>
2025-01-27 14:57:14 -0300

    Add files via upload

---
+++ js/index.ae312417.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("FEB 22, 2025 20:00:00").getTime(),t=setInterval(function(){let n=e-new Date().getTime(),o=Math.floor(n/864e5),a=Math.floor(n%864e5/36e5),r=Math.floor(n%36e5/6e4),l=Math.floor(n%6e4/1e3);document.getElementById("contador").innerHTML=`${o}d ${a}h ${r}m ${l}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Evento expirado")},1e3);
+//# sourceMappingURL=index.ae312417.js.map
